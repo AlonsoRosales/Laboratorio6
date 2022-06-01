6924491b452cb7b2bf8954eac1a0b60cb3ad319b
Alonso Rosales <a20191566@pucp.edu.pe>
2022-05-31 19:26:42 -0500

    fiu fiu

--- src/main/java/com/example/lab6/Controllers/Pregunta1.java
+++ src/main/java/com/example/lab6/Controllers/Pregunta1.java
@@ -1,9 +1,18 @@
 package com.example.lab6.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Pregunta1 {
+
+
+
+    @GetMapping("")
+    public String home(){
+        return "pregunta1";
+    }
+
 
 
 
