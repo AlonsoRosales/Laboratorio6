dadffd53325bf4b1679f7622fce8697f84e70679
Ronny Pastor <ronnypastorkolmakov@gmail.com>
2022-05-31 20:25:52 -0500

    Controller

--- src/main/java/com/example/lab6/Controllers/PreguntasController.java
+++ src/main/java/com/example/lab6/Controllers/PreguntasController.java
@@ -1,5 +1,6 @@
 package com.example.lab6.Controllers;
 
+import com.example.lab6.Entity.Employee;
 import com.example.lab6.Repository.EmployeeReporsitory;
 import com.example.lab6.Repository.JobReporsitory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @Controller
 public class PreguntasController {
@@ -33,13 +39,18 @@ public class PreguntasController {
 
     @GetMapping("/Pregunta2b/list")
     public String preguntita2b(Model model){
-        model.addAttribute("listaEmpleados",employeeReporsitory.findAll());
+        model.addAttribute("listaEmpleados",employeeReporsitory.empleadosIT());
         return "pregunta2b/list";
     }
     @GetMapping("/Pregunta2b/new")
     public String crearNuevoEmpleado(Model model){
-
+        model.addAttribute("listaTrabajos",jobReporsitory.findAll());
         return "pregunta2b/newForm";
+    }
+    @PostMapping("/Pregunta2b/save")
+    public String guardarEmpleado(Employee employee){
+        employeeReporsitory.save(employee);
+        return "redirect:/pregunta2b/list";
     }
 
 
--- src/main/java/com/example/lab6/Repository/EmployeeReporsitory.java
+++ src/main/java/com/example/lab6/Repository/EmployeeReporsitory.java
@@ -2,8 +2,13 @@ package com.example.lab6.Repository;
 
 import com.example.lab6.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeReporsitory extends JpaRepository<Employee, Integer> {
+    @Query(nativeQuery = true,value = "SELECT * FROM hr.employees WHERE hr.employees.department_id = 60;" )
+    List<Employee> empleadosIT();
 }
